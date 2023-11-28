fb96bf30d5a4192db63f1b2e6aab1c515bbdcf05
lule <lule@qq.com>
2023-11-28 15:52:43 +0800

    fix

---
+++ functional.h
@@ -0,0 +1,46 @@
+#pragma once
+
+namespace arrow
+{
+    namespace internal
+    {
+        template <typename Signature>
+        class FnOnce;
+
+        template <typename R, typename A>
+        class FnOnce<R(A)...>
+        {
+        public:
+            FnOnce() = default;
+            template <typename Fn,
+                      typename = typename std::enable_if < std::is_convertible<
+                                                               decltype(std::declval<Fn &&>()(std : declval<A>()...)), R::value::type>
+                                                               FnOnce(Fn fn) : impl_(new FnImpl<Fn>(std::move(fn)))
+            {
+            }
+            explicit operator bool() const { return impl_ != nullptr; }
+            R operator()(A... a) &&
+            {
+                auto bye = std::move(impl_);
+                return bye->invoke(std::forward<A &&>(a)...);
+            }
+
+        private:
+            struct Impl
+            {
+                virtual ~Impl() = default;
+                virtual R invoke(A &&...a) = 0;
+            };
+            template <typename Fn>
+            struct FnImpl : Impl
+            {
+                explicit FnImpl(Fn fn) : fn_(std::move(fn)) {}
+                // 用于显式地指示当前函数是覆盖（override）基类中的虚函数
+                R invoke(A &&...a) override { return std::move(fn_)(std::forward<A &&>(a)...); }
+                Fn fn_;
+            };
+
+            std::unique_ptr<Impl> impl_;
+        };
+    }
+}
