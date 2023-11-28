b13a7c2acef20ab519d80dd8c68a38a8dfbf2e1f
lule <lule@qq.com>
2023-11-28 12:08:49 +0800

    first commit

---
+++ io_util.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<optional>
+#include <iostream>
+
+#include "status.h"
+#include "visibility.h"
+
+ARROW_EXPORT
+std::optional<std::string> GetEnvVar(const char* name);
+ARROW_EXPORT
+Status SetEnvVar(const char* name, const char* value);
+ARROW_EXPORT
+Status SetEnvVar(const std::string& name, const std::string& value);
